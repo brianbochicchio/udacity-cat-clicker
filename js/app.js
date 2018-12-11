8e802698c394f969c3934107d28809358972ae15
Brian Bochicchio <bbochicchio@gmail.com>
2018-12-11 00:38:26 -0600

    docs: added comment to click event handler

--- js/app.js
+++ js/app.js
@@ -34,7 +34,9 @@ kittehName2.innerHTML = cats[1].name;
 // Increment the value only when an image is clicked
 theKittehs.addEventListener('click', function(e){
     
-    if (e.target.tagName === "IMG"){  
+    if (e.target.tagName === "IMG"){ 
+        // loop through cats and see if the event's src element contains cat's the filename
+        // if it does increment the count and update the display
         for (let i = 0; i < cats.length; i++){
             if(e.srcElement.currentSrc.includes(cats[i].imageName) === true) {
                 cats[i].clicks += 1;
